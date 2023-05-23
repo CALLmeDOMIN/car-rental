a62880ec0d52036718b80de1d29588184e5bfcdd
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-05-24 00:04:59 +0200

    basic fetching and basic navbar

--- src/app/car/[id]/page.tsx
+++ src/app/car/[id]/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-    return <div>My Post</div>;
+import { PrismaClient } from "@prisma/client";
+import Image from "next/image";
+
+const prisma = new PrismaClient();
+
+type Params = {
+    id: number;
+};
+
+export default async function Page({ params }: { params: Params }) {
+    const id = Number(params.id);
+    const data = await prisma.car.findUnique({ where: { id } });
+    const imageSrc = "/" + data?.id + ".jpg";
+
+    return (
+        <div>
+            Car {id}:{/* {print out data wrapped in h1's} */}
+            <h1>{data?.name}</h1>
+            <h1>{data?.brand}</h1>
+            <h1>${data?.price}</h1>
+            <h1>{data?.year}</h1>
+            <Image src={imageSrc} width={200} height={100} alt="carimg" />
+        </div>
+    );
 }
