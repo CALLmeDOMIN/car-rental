8d95a3e4c51c79cf9e5aae41a1c4bb6b8c36c03f
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-06-24 23:12:34 +0200

    ESLint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-    publicRoutes: ["/", "/cars", "/cars/(.*)", "/contact"],
-});
+    publicRoutes: ['/', '/cars', '/cars/(.*)', '/contact'],
+})
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
