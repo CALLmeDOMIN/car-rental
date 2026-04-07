8126f438855f88a4e8dd6080ebaafb2ae893107e
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2026-04-07 23:47:48 +0200

    fix: linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,9 @@
-import nextTypescript from "eslint-config-next/typescript";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import ts from "typescript-eslint";
-import prettierConfigRecommended from "eslint-plugin-prettier/recommended";
-import { fixupConfigRules } from "@eslint/compat";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const patchedConfig = fixupConfigRules([
-  ...nextCoreWebVitals,
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
-const config = [
-  ...nextTypescript,
-  ...patchedConfig,
-  ...ts.configs.recommended,
-  prettierConfigRecommended,
-  { ignores: [".next/*"] }
-];
-
-export default config;
+export default eslintConfig;
